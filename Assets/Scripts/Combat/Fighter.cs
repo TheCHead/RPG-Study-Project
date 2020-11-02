cf6662d60d67d027690b07fcc1a174a742d8feb9
Nikita Dmitriev <nikdmit8@gmail.com>
2020-11-02 12:07:55 +0300

    Initial Commit: - Terrain and environment - Basic movement (point-click movement, blend animations) - Start Attack action (identify enemy, move within enemy range, cancel attack with movement)

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,39 @@
+﻿using RPG.Movement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+        Transform target;
+        [SerializeField] float weaponRange = 2f;
+
+        private void Update()
+        {
+            if (target != null)
+            {
+                GetComponent<Mover>().MoveTo(target.position);
+                if (Vector3.Distance(transform.position, target.position) <= weaponRange)
+                {
+                    GetComponent<Mover>().StopMoving();
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        public void Attack(CombatTarget combatTarget)
+        {
+            target = combatTarget.transform;
+        }
+
+        public void DropTarget()
+        {
+            target = null;
+        }
+    }
+}
---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,53 @@
+﻿using RPG.Combat;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour
+    {
+        [SerializeField] Transform target;
+        Ray lastRay;
+        NavMeshAgent myNavMeshAvent;
+
+        private void Start()
+        {
+            myNavMeshAvent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update()
+        {
+            UpdateAnimator();
+        }
+
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<Fighter>().DropTarget();
+            MoveTo(destination);
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            myNavMeshAvent.destination = destination;
+            myNavMeshAvent.isStopped = false;
+        }
+
+        public void StopMoving()
+        {
+            myNavMeshAvent.isStopped = true;
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+
+            GetComponent<Animator>().SetFloat("forwardSpeed", localVelocity.z);
+        }
+
+    }
+}
+
+
