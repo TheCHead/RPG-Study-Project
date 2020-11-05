9f78e37318786d57014828b621e45b37217abaf9
Nikita Dmitriev <nikdmit8@gmail.com>
2020-11-05 18:36:50 +0300

    Added patrol paths

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        [SerializeField] float waypointGizmoRadius = 0.3f;
+
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                if (i < transform.childCount - 1)
+                {
+                    Gizmos.color = Color.grey;
+                    Gizmos.DrawSphere(transform.GetChild(i).position, waypointGizmoRadius);
+                    Gizmos.DrawLine(transform.GetChild(i).position, transform.GetChild(i + 1).position);
+                }
+                else
+                {
+                    Gizmos.DrawSphere(transform.GetChild(i).position, waypointGizmoRadius);
+                    Gizmos.DrawLine(transform.GetChild(i).position, transform.GetChild(0).position);
+                }
+            }
+        }
+    }
+}
+
+
