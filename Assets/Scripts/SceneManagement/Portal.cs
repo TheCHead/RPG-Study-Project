cf42432851e639e6734bc0978cafca60cc0f3234
Nikita Dmitriev <nikdmit8@gmail.com>
2020-11-24 16:09:52 +0300

    2nd scene, basic portals between scenes

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] int sceneToLoad;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(Transition());
+        }
+    }
+
+    private IEnumerator Transition()
+    {
+        DontDestroyOnLoad(gameObject);
+        yield return SceneManager.LoadSceneAsync(sceneToLoad);
+        print("scene loaded");
+        Destroy(gameObject);
+    }
+}
